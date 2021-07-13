569e067a09ab940196b39b3a885901aefd973cb3
IM-RAHUL-RAJ <rahuldps3313@gmail.com>
2021-07-13 17:54:05 +0530

    introduction

---
+++ hello.py
@@ -0,0 +1,13 @@
+import flask  #importing the flask module
+# before import any external module run pip install 
+
+print("hello world")
+
+
+# //write pyhton in terminal to start the Python REPL
+
+'''multiline comment . Thanks for reading'''
+
+
+
+
